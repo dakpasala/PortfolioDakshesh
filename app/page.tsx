2cb16059381af2b3fcd6441212d9106cef982ae6
Dakshesh <dpasala@calpoly.edu>
2024-12-14 15:33:30 -0800

    Dynamically added Grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,25 @@
-import Approach from "@/components/Approach";
-import Client from "@/components/Client";
-import Experience from "@/components/Experience";
-import Footer from "@/components/Footer";
-import Grid from "@/components/Grid";
-import Hero from "@/components/Hero";
-import RecentProjects from "@/components/RecentProjects";
-import Skills from "@/components/Skills";
+import dynamic from "next/dynamic";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import Hero from "@/components/Hero";
+import Skills from "@/components/Skills";
+import RecentProjects from "@/components/RecentProjects";
+import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 import { navItems } from "@/data";
+
+// Dynamically import Grid
+const Grid = dynamic(() => import("@/components/Grid"), { ssr: false });
 
 export default function Home() {
   return (
     <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav 
-        navItems={navItems} />
+        <FloatingNav navItems={navItems} />
         <Hero />
-        {/*<Grid />*/}
+        <Grid />
         <Skills />
-        <RecentProjects /> 
-        {/* <Client /> */}
+        <RecentProjects />
         <Experience />
         <Approach />
         <Footer />
